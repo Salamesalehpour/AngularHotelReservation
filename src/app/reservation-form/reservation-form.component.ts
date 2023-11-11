ba0aef467146e15f891382c57bae43d413c469a5
Salam Salehpour <salamesalehpour@gmail.com>
2023-11-12 00:42:53 +0330

    Add service module

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validator, Form, Validators} from "@angular/forms";
+import {Reservation} from "../models/reservation";
+import {ReservationService} from "../reservation/reservation.service";
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit{
+  reservationForm: FormGroup = new FormGroup({})
 
+  constructor(private formBuilder: FormBuilder, private reservationService: ReservationService) {
+  }
+  ngOnInit() {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    if (this.reservationForm.valid){
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation);
+    }
+  }
 }
